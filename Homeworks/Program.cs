b279149b6b51dd0886b17ad282eb10fa9f3b0a1a
Avet Xachatryan <AvetKhachatryan24@gmail.com>
2024-04-26 15:31:52 +0400

    All done

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿//2a
+//int[,] arr = new int[5, 5];
+//Random r = new Random();
+//int sum = 0;
+//for (int i = 0; i < 5; i++)
+//{
+//    for (int j = 0; j < 5; j++)
+//    {
+//        arr[i, j] = r.Next(0, 10);
+//        sum += arr[i, j];
+//        Console.Write(arr[i, j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine("Gumary: " + sum);
+
+
+//2b
+//int[,] arr = new int[5, 5];
+//Random r = new Random();
+//for (int i = 0; i < 5; i++)
+//{
+//    int sum = 0;
+//    for (int j = 0; j < 5; j++)
+//    {
+//        arr[i, j] = r.Next(0, 10);
+//        sum += arr[i, j];
+//        Console.Write(arr[i, j] + " ");
+//    }
+//    Console.WriteLine("Gumary: " + sum);
+//}
+
+
+//2g
+//int[,] arr = new int[5, 5];
+//Random r = new Random();
+//int count = 0;
+//for (int i = 0; i < 5; i++)
+//{
+//    bool t = true;
+//    for (int j = 0; j < 5; j++)
+//    {
+//        arr[i, j] = r.Next(1, 10);
+//        Console.Write(arr[i, j] + " ");
+//        if (arr[i, j] < 5)
+//        {
+//            t = false;
+//        }
+//    }
+//    if (t)
+//    {
+//        count++;
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine("Qanaky: " + count);
+
+
+
