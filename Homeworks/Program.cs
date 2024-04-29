2620e39b870b3385c9eb993ce996cc66f547a97c
Avet Xachatryan <AvetKhachatryan24@gmail.com>
2024-04-29 16:52:02 +0400

    Done

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.IO;
+
+string pathOut = @"C:\Users\avetx\Desktop\C##\example.txt";
+string pathIn = @"C:\Users\avetx\Desktop\Folder\newpage.txt";
+string line;
+using (StreamReader sr = new StreamReader(pathOut))
+{
+    line = sr.ReadLine()!;
+    while (line != null)
+    {
+        using (StreamWriter sw = new StreamWriter(pathIn, true))
+        {
+            sw.WriteLine(line);
+        }
+        line = sr.ReadLine()!;
+    }
+}
