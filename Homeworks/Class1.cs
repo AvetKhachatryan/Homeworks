72dfed218447b15ab65671336b5a66cccec9859d
Avet Xachatryan <AvetKhachatryan24@gmail.com>
2024-05-11 22:59:41 +0400

    Done

---
+++ Homeworks/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks
+{
+    public class Class1
+    {
+        //xndir 1
+        public List<int> MergeSort(int[] arr1, int[] arr2)
+        {
+            List<int> result = new List<int>();
+            foreach(int i in arr1)
+            {
+                result.Add(i);
+            }
+            foreach(int i in arr2)
+            {
+                result.Add(i);
+            }
+            for (int i = 0; i < result.Count; i++)
+            {
+                for (int j = i; j < result.Count; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        int temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+            return result;
+        }
+        public void printResult(List<int> result)
+        {
+            foreach(int i in result)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        //xndir 2
+        public static int ObjCount = 0;
+        public Class1()
+        {
+            ObjCount++;
+        }
+    }
+}
--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,1 +1,15 @@
-﻿
+using Homeworks;
+
+//xndir 1
+int[] arr1 = { 1, 45, 0 };
+int[] arr2 = { 4, 11, 111 };
+
+Class1 ob = new Class1();
+ob.printResult(ob.MergeSort(arr1, arr2));
+
+//xndir2
+Class1 ob1 = new Class1();
+Class1 ob2 = new Class1();
+Class1 ob3 = new Class1();
+
+Console.WriteLine(Class1.ObjCount);
